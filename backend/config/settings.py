ead46caaac0fd2aeb7b8ebd0e4a646221d117d43
Stephan Riley <stephan.riley1@ibm.com>
2026-04-01 18:19:41 -0600

    Remove default static files value

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = os.getenv('STATIC_URL', '/django-static/')
+STATIC_URL = os.getenv('STATIC_URL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
