5bc6c411cd6a54f82ba54569751196ff93b71ae6
Stephan Riley <stephan.riley1@ibm.com>
2026-04-01 21:17:35 -0600

    remove SessionAuthentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -163,7 +163,6 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
